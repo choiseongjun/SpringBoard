3e7240c5228475d8bc8c4e6d3c1a6d4b6b8dac81
csj2702 <csj2702@naver.com>
2018-08-17 13:56:43 +0900

    BackgroundSetting

---
+++ src/main/java/jun/st/ex/MainController.java
@@ -0,0 +1,25 @@
+package jun.st.ex;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class MainController {
+	
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home() {
+		
+		
+		return "Main";
+	}
+	
+}
